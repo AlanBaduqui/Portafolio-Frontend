5f2e8f4c772097f9f5c4ca0bb84844396e013764
Alan <alanbaduqui@hotmail.com>
2020-08-30 15:58:02 -0700

    Actualizacion

---
+++ src/Component/Portafolio/Portafolio.js
@@ -0,0 +1,13 @@
+import React, {useState,useEffect} from 'react';
+import axios from 'axios';
+import './Portafolio.css';
+
+function Portafolio(){
+
+    const [information, setInformation] = useState({});
+    useEffect(()=>{
+        axios.get
+    })
+
+    return()
+};
