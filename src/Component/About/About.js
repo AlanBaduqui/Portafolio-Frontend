88b11445ad4572dde617cbb8d9a30e8c4feb2ebe
Alan <alanbaduqui@hotmail.com>
2020-09-08 08:47:21 -0700

    first commit

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import axios from 'axios';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Container from '@material-ui/core/Container';
+import Button from '@material-ui/core/Button';
 import './About.css'
 
 function About() {
@@ -15,32 +15,30 @@ function About() {
                 res => setInformation(res.data[0])
             )
     }, [setInformation]);
-    
+
     return (
         <div className="About">
             <Container maxWidth="sm">
                 <Card variant="outlined">
-                    <CardActionArea>
+                    <CardContent maxWidth="sm">
                         <div className="div-foto">
                             <img className="foto-perfil" alt="FotoPerfil" src={information.photo}></img>
                         </div>
-                    </CardActionArea>
-                    <CardContent maxWidth="sm">
                         <div className="Descripcion">
                             <h1>{information.name}</h1>
                             <h2>{information.profession}</h2>
                             <p>{information.about_me}</p>
                             <ul className="skills">
                                 {information.skills && information.skills.map((skill, index) =>
-                                    <p className="skill">{skill}</p>
+                                    <Button size="small" color="primary">
+                                        <p className="skill">{skill}</p>
+                                    </Button>
                                 )}
                             </ul>
                         </div>
                     </CardContent>
                 </Card>
             </Container>
-
-
         </div>
     )
 };
